e3197f2bfbf278d0cfa554c78c70c2c018e7abed
Markus Rudy <mr@edgeless.systems>
2026-04-20 13:19:09 +0200

    make it work on k3s
    * privileged somehow messed with networking
    * unprivileged (but with caps) breaks `ip netns`
    
    => implement the pedestrian way, with nsenter

--- setup.sh
+++ setup.sh
@@ -2,54 +2,69 @@
 set -x
 set -euo pipefail
 
-while [[ -z "$(ip route)" ]]; do
+touch /tmp/ready
+
+# Wait for network to actually be configured.
+until ip route show default | grep -q default; do
   printf "%s Waiting for network configuration\n" "$(date)"
+  ip addr || true
+  ip route || true
   sleep 1
 done
 
-# Network namespace 'vm' simulates the Kata VM.
-ip netns add vm
+ip route
+ip route show default
+ip -j addr show eth0 | jq
 
-# tap0 simulates the tap device used by the hypervisor.
+# Create "vm" network namespace.
+unshare -n -- bash -c 'sleep infinity' &
+vm_pid=$!
+
+# Give it a moment to exist.
+sleep 0.5
+
+# tap0 simulates the tap device used by the hypervisor, veth0 will simulate the guest's main interface.
 ip link add tap0 type veth peer name veth0
-ip link set veth0 netns vm
-ip -n vm link set dev veth0 name eth0
+ip link set veth0 netns "$vm_pid"
+nsenter -t "$vm_pid" -n ip link set dev veth0 name eth0
 
-# Save default route
+# Setting the route in the guest requires the IPs to be configured, but removing the IP on the host removes the route.
+# Save it now and use it afterwards.
 defaultRoute=$(ip route show default | head -n1)
 
-# Copy address configuration from host to guest.
-ip -n vm addr add dev lo 127.0.0.1/8
-ip -n vm addr add dev lo ::1/128
+# Configure loopback inside guest.
+nsenter -t "$vm_pid" -n ip addr add 127.0.0.1/8 dev lo
+nsenter -t "$vm_pid" -n ip addr add ::1/128 dev lo
 
+# Copy IP addresses from host eth0 to namespace eth0.
 for addr in $(ip -j addr show eth0 | jq -r '.[] | .addr_info[] | "\(.local)/\(.prefixlen)"'); do
-  ip -n vm addr add dev eth0 $addr
-  ip addr del dev eth0 $addr
+  nsenter -t "$vm_pid" -n ip addr add dev eth0 "$addr"
+  ip addr del dev eth0 "$addr"
 done
-ip -n vm link set dev eth0 address $(ip -j link show eth0 | jq -r '.[0].address')
 
-# Set the link pair up, otherwise routes won't work.
-ip -n vm link set lo up
-ip -n vm link set eth0 up
+# Copy MAC address.
+mac=$(ip -j link show eth0 | jq -r '.[0].address')
+nsenter -t "$vm_pid" -n ip link set dev eth0 address "$mac"
+
+# Bring interfaces up.
+nsenter -t "$vm_pid" -n ip link set lo up
+nsenter -t "$vm_pid" -n ip link set eth0 up
 ip link set tap0 up
 
-# Copy the default route (should be enough for the demo).
-ip -n vm route add $defaultRoute
+# Add default route inside guest.
+nsenter -t "$vm_pid" -n ip route add $defaultRoute
 
-# Set up tc redirection.
+# Set up tc redirection (host side).
 tc qdisc add dev eth0 ingress
 tc filter add dev eth0 ingress matchall \
     action mirred egress redirect dev tap0
+
 tc qdisc add dev tap0 ingress
 tc filter add dev tap0 ingress matchall \
     action mirred egress redirect dev eth0
 
-ip addr show eth0
-
-pushd "$(dirname "${BASH_SOURCE[0]}")"
-ip netns exec python3 -m http.server 8080 &
+# Start HTTP server inside the namespace
+nsenter -t "$vm_pid" -n python3 -m http.server 8080 &
 
 touch /tmp/ready
 wait
-# bash -i
-# tcpdump -ni any
